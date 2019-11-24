ad33f0033bac687db2745c6edde1d752544a9eb0
Benoit Mariaux <benoit.mariaux@saint-gobain.com>
2019-11-24 22:34:22 +0100

    sse-c wip

--- s3-encryption/sse-c.py
+++ s3-encryption/sse-c.py
@@ -1,9 +1,21 @@
 import boto3
 import uuid
 import os
+import sys
 
 # SSE-C
 # Server side encryption, but Custom encryption key provided
+
+# debug
+# import sys
+
+# file_object = open('/tmp/tttt', "r+")
+# for item in range(1,100):
+#     file_object.write("A text file: " + str(item) + "\n")
+    
+
+
+# sys.exit();
 
 BUCKET = "sse-c-demo-"+str(uuid.uuid4())
 
@@ -16,9 +28,11 @@ s3.create_bucket(Bucket=BUCKET)
 FILE = "/tmp/sse-c-file"
 OBJECT_KEY=os.path.basename(FILE)
 
-f = open(FILE, "w")
-f.write("A text file\n")
-f.close()
+file_object = open(FILE, "w")
+for i in range(100):
+    file_object.write("Line %d\n" % (i+1))
+
+file_object.close()
 
 # Generate a random 32 bytes (256 bits) 
 SECRET_KEY = os.urandom(32)
@@ -26,9 +40,13 @@ SECRET_KEY = os.urandom(32)
 s3.put_object(
     Bucket=BUCKET,
     Key=OBJECT_KEY,
+    Body=open(FILE, "r").read(),
     SSECustomerKey=SECRET_KEY,
     SSECustomerAlgorithm='AES256'
 )
+
+file_object.close()
+
 
 # Check object encryption
 r = s3.head_object(
@@ -48,9 +66,11 @@ r = s3.get_object(
     SSECustomerAlgorithm='AES256'
 )
 
+print(r['Body'].read())
+
 # TODO
-r = s3.download_file(BUCKET, OBJECT_KEY, FILE)
-print(r)
+#r = s3.download_file(BUCKET, OBJECT_KEY, FILE)
+
 
 
 
